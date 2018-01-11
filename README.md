ca59c60c6a5ea7abb44c56e7a2d7d129ff22d8d6
Theo Salzmann <dev@on3iro.de>
2018-01-11 13:21:56 +0100

    wip

---
+++ README.md
@@ -0,0 +1,14 @@
+# ToC
+
+<!-- vim-markdown-toc GFM -->
+
+* [npm-lib-starter](#npm-lib-starter)
+    * [Versioning](#versioning)
+
+<!-- vim-markdown-toc -->
+
+# npm-lib-starter
+
+## Versioning
+
+We use [SemVer](http://semver.org/) for versioning.
---
+++ hooks/post_gen_project.py
@@ -0,0 +1,42 @@
+import subprocess
+import os
+from sys import version_info
+
+py3 = version_info[0] > 2
+
+if py3:
+    input_fnc = input
+else:
+    input_fnc = raw_input
+
+init_git = input_fnc('Initiate new git repository? [YES][no]')
+
+if init_git.lower() == 'no':
+    pass
+else:
+    subprocess.call(['git', 'init'])
+
+
+install_npm = input_fnc('Install npm dependencies? [YES][no]')
+
+if install_npm.lower() == 'no':
+    pass
+else:
+    subprocess.call('npm i', shell=True)
+
+if init_git.lower() == 'no':
+    pass
+else:
+    initial_commit = input_fnc('Create initial commit? [YES][no]')
+
+    if initial_commit.lower() == 'no':
+        pass
+    else:
+        subprocess.call(['git', 'add', './'])
+        subprocess.call(['git', 'commit', '-m', '"Initial Commit"'])
+
+    print('Adding remote origin {{cookiecutter.repo_url}}...')
+    subprocess.call(['git', 'remote', 'add', 'origin', '{{ cookiecutter.repo_url }}'])
+
+    print('Checking out dev branch')
+    subprocess.call(['git', 'checkout', '-b', 'dev'])
